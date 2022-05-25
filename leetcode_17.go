2f00549e0c75f1773972dabdf1d44e6bdf1c98e5
Zipeng Chen <zipeng.chen@shopee.com>
2022-05-25 23:49:05 +0800

    1. leetcode 17

---
+++ leetcode_17.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	result := letterCombinations("23")
+	fmt.Println(result)
+}
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 || len(digits) > 4 ||
+		digits == "" || digits == "1" || digits == "0" ||
+		digits == "*" || digits == "#" {
+		return []string{}
+	}
+	res6 := make([]string, 0)
+	digitsMap := [10]string{
+		"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
+	}
+	backtracking6(digits, "", 0, digitsMap, &res6)
+	return res6
+}
+
+func backtracking6(digits, temp string, idx int, digitsMap [10]string, res6 *[]string) {
+	if idx == len(digits) {
+		*res6 = append(*res6, temp)
+		return
+	}
+	num := digits[idx] - '0'
+	chars := digitsMap[num]
+	for i := 0; i < len(chars); i++ {
+		temp = temp + string(chars[i])
+		backtracking6(digits, temp, idx+1, digitsMap, res6)
+		temp = temp[:len(temp)-1]
+	}
+}
