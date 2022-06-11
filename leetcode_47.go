d6a5edcda09c11d16f3e9f185d4037354584fad3
Zipeng Chen <zipeng.chen@shopee.com>
2022-06-11 16:31:01 +0800

    1. review leetcode 47;

--- leetcode_47.go
+++ leetcode_47.go
@@ -6,37 +6,36 @@ import (
 )
 
 func main() {
-	nums := []int{1, 1, 2}
-	res := permuteUnique(nums)
+	res := permuteUnique([]int{1, 1, 2})
 	fmt.Println(res)
 }
 
 func permuteUnique(nums []int) [][]int {
 	sort.Ints(nums)
 	res := make([][]int, 0)
-	used := make([]bool, len(nums))
-	dfs10(nums, []int{}, used, &res)
+	used := make([]bool, len(nums), len(nums))
+	var backtracking func(temp []int)
+	backtracking = func(temp []int) {
+		if len(temp) == len(nums) {
+			tmp := make([]int, len(temp))
+			copy(tmp, temp)
+			res = append(res, tmp)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
+				continue
+			}
+			if used[i] {
+				continue
+			}
+			used[i] = true
+			temp = append(temp, nums[i])
+			backtracking(temp)
+			temp = temp[:len(temp)-1]
+			used[i] = false
+		}
+	}
+	backtracking([]int{})
 	return res
-}
-
-func dfs10(nums, temp []int, used []bool, res *[][]int) {
-	if len(nums) == len(temp) {
-		tmp := make([]int, len(temp))
-		copy(tmp, temp)
-		*res = append(*res, tmp)
-		return
-	}
-	for i := 0; i < len(nums); i++ {
-		if used[i] {
-			continue
-		}
-		if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
-			continue
-		}
-		used[i] = true
-		temp = append(temp, nums[i])
-		dfs10(nums, temp, used, res)
-		temp = temp[:len(temp)-1]
-		used[i] = false
-	}
 }
