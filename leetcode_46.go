1243dfdc5311a61c852748450a943e5f99e807dd
Zipeng Chen <zipeng.chen@shopee.com>
2022-06-11 16:38:23 +0800

    1. review leetcode 46;

--- leetcode_46.go
+++ leetcode_46.go
@@ -5,33 +5,32 @@ import (
 )
 
 func main() {
-	nums := []int{1, 2, 3}
-	res := permute(nums)
+	res := permute([]int{1, 2, 3})
 	fmt.Println(res)
 }
 
 func permute(nums []int) [][]int {
 	res := make([][]int, 0)
-	used := make([]bool, len(nums))
-	dfs9(nums, []int{}, used, &res)
-	return res
-}
-
-func dfs9(nums, temp []int, used []bool, res *[][]int) {
-	if len(temp) == len(nums) {
-		tmp := make([]int, len(temp))
-		copy(tmp, temp)
-		*res = append(*res, tmp)
-		return
-	}
-	for i := 0; i < len(nums); i++ {
-		if used[i] {
-			continue
+	used := make([]bool, len(nums), len(nums))
+	var backtracking func([]int)
+	backtracking = func(temp []int) {
+		if len(temp) == len(nums) {
+			tmp := make([]int, len(temp))
+			copy(tmp, temp)
+			res = append(res, tmp)
+			return
 		}
-		used[i] = true
-		temp = append(temp, nums[i])
-		dfs9(nums, temp, used, res)
-		temp = temp[:len(temp)-1]
-		used[i] = false
+		for i := 0; i < len(nums); i++ {
+			if used[i] {
+				continue
+			}
+			used[i] = true
+			temp = append(temp, nums[i])
+			backtracking(temp)
+			temp = temp[:len(temp)-1]
+			used[i] = false
+		}
 	}
+	backtracking([]int{})
+	return res
 }
