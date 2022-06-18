8a7e2703043e30652cd577dbbf7e83b1d7f45961
Zipeng Chen <zipeng.chen@shopee.com>
2022-06-19 00:53:01 +0800

    1. review leetcode 37;

--- leetcode_37.go
+++ leetcode_37.go
@@ -5,44 +5,46 @@ func main() {
 }
 
 func solveSudoku(board [][]byte) {
-	var backtracking func(board [][]byte) bool
-	backtracking = func(board [][]byte) bool {
-		for i := 0; i < 9; i++ {
-			for j := 0; j < 9; j++ {
-				if board[i][j] != '.' {
-					continue
-				}
-				for k := '1'; k <= '9'; k++ {
-					if !isValid4(board, byte(k), i, j) {
-						continue
-					}
-					board[i][j] = byte(k)
-					if backtracking(board) {
-						return true
-					}
-					board[i][j] = '.'
-				}
-				return false
-			}
-		}
-		return true
-	}
-	backtracking(board)
+	backtracking11(board)
 }
 
-func isValid4(board [][]byte, val byte, x, y int) bool {
+func backtracking11(board [][]byte) bool {
 	for i := 0; i < 9; i++ {
-		if board[i][y] == val {
+		for j := 0; j < 9; j++ {
+			if board[i][j] != '.' {
+				continue
+			}
+			for k := '1'; k <= '9'; k++ {
+				if !isValid4(board, byte(k), i, j) {
+					continue
+				}
+				board[i][j] = byte(k)
+				if backtracking11(board) {
+					return true
+				}
+				board[i][j] = '.'
+			}
 			return false
 		}
 	}
+	return true
+}
+
+func isValid4(board [][]byte, val byte, row, col int) bool {
 	for i := 0; i < 9; i++ {
-		if board[x][i] == val {
+		if board[row][i] == val {
 			return false
 		}
 	}
-	startRow := (x / 3) * 3
-	startCol := (y / 3) * 3
+
+	for i := 0; i < 9; i++ {
+		if board[i][col] == val {
+			return false
+		}
+	}
+
+	startRow := (row / 3) * 3
+	startCol := (col / 3) * 3
 	for i := startRow; i < startRow+3; i++ {
 		for j := startCol; j < startCol+3; j++ {
 			if board[i][j] == val {
